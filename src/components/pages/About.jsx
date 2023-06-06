b237f1dd0f97b388c47b2d2fb6b9a50ccf336930
Johnathon Forbush <jtforbush429@gmail.com>
2023-06-06 17:22:34 -0500

    Fixed Bug

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -5,7 +5,7 @@ function About() {
         <div className="container">
             <div className="card mt-4">
                 <div className="card-body">
-                    <h2>Home Page</h2>
+                    <h2>About Page</h2>
                 </div>
             </div>
         </div>
