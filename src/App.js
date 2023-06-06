338394c8619d2182d42c6ca35e1f2ba8dccf28f9
Johnathon Forbush <jtforbush429@gmail.com>
2023-06-06 17:40:21 -0500

    Prototype Contact Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import React from 'react';
+import Header from './Header'
 import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from './components/pages/Contact';
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Header />
+      <Routes>
+        <Route exact path="" element={<Home />}/>
+        <Route exact path="/About" element={<About />}/>
+        <Route exact path="/Contact" element={<Contact />}/>
+      </Routes>
     </div>
   );
 }
