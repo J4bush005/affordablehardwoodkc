6b8a5f36770f17693ea87367189f4fad585e3282
Johnathon Forbush <jtforbush429@gmail.com>
2023-06-07 21:37:45 -0500

    Working on Navbar

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Header from './Header'
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Contact from './components/pages/Contact';
-import {Route, Routes} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Home />
-      <About />
-      <Contact />
-    </div>
-  );
+    <Router>
+      <div>
+        <Header />
+      </div>
+    </Router>
+  )
+
 }
 
 export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,35 +1,37 @@
+import "./index.css";
 import React from "react";
+import FloorLogo from "./components/images/floor logo.png";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
 function Header(){
     return(
- <nav class="navbar navbar-expand-lg bg-body-tertiary">
+ <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">Navbar</a>
+   <Link to="/" id="words" class="navbar-brand" >
+      <img id="logo" src={FloorLogo} alt=""></img>
+      
+            Kansas City's Hardwood Specialist
+    </Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="/About" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            About
-          </a>
+            <Link to="/About" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">About</Link>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/About">Action</a></li>
-            <li><a class="dropdown-item" href="/">Another action</a></li>
-            <li><a class="dropdown-item" href="/">Something else here</a></li>
+            <li><Link to="/About" class="nav-link " role="button" aria-expanded="false">Action</Link></li>
+            <li><Link to="/About" class="nav-link " role="button" aria-expanded="false">Another action</Link></li>
+            <li><Link to="/About" class="nav-link " role="button" aria-expanded="false">Something else here</Link></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-           Contact
-          </a>
+          <Link to="/Contact" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Contact</Link>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/">Action</a></li>
-            <li><a class="dropdown-item" href="/">Another action</a></li>
-            <li><a class="dropdown-item" href="/">Something else here</a></li>
+            <li><Link to="/Contact" class="nav-link" role="button"  aria-expanded="false">Action</Link></li>
+            <li><Link to="/Contact" class="nav-link" role="button"  aria-expanded="false">Another action</Link></li>
+            <li><Link to="/Contact" class="nav-link" role="button"  aria-expanded="false">Something else here</Link></li>
           </ul>
         </li>
       </ul>
