82d7679913e877a15e89b43419934cee5ca8f65a
Johnathon Forbush <jtforbush429@gmail.com>
2023-06-09 18:08:52 -0500

    Styled the hamburger menu

--- src/Header.jsx
+++ src/Header.jsx
@@ -23,7 +23,7 @@ function Header(){
       
         </Link>
           
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
