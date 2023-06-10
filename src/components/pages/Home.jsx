e8ac69b06c90317815b1f71859774964a15fac8d
Johnathon Forbush <jtforbush429@gmail.com>
2023-06-10 16:37:37 -0500

    Working on adding picture to animation

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -20,7 +20,7 @@ import Demo5 from "../images/Demo5.png";
 import Demo6 from "../images/Demo6.png";
 import { Link } from "react-router-dom";
 
-const FadeUp = batch(Fade(), Sticky(35), Move());
+const FadeUp = batch(Fade(), Sticky(38), Move());
 const FadeUp2 = batch(Fade(), Sticky(), Move());
 
 function Home() {
@@ -98,20 +98,43 @@ function Home() {
       <div className="container-fluid">
         <ScrollPage page={0}>
           <Animator animation={FadeUp}>
-            <div className="row">
-              <div className="col-12">
-                <div id="open-heading" className="1-text heading-text">
-                  Providing Superior Quality
+            <div class="row">
+              <div class="col-12 col-lg-6">
+                <div class="row">
+                  <div class="col-12">
+                    <div id="open-heading" class="1-text heading-text">
+                      Providing Superior Quality
+                    </div>
+                    <div id="open-heading" class="1-text heading-text">
+                      At An Affordable Price
+                    </div>
+                    <div class="flex">
+                      <div class="x1 goldbar home"></div>
+                      <div class="x1"></div>
+                    </div>
+                    <div class="r-text paragraph">
+                      Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                      Maecenas porttitor congue massa. Fusce posuere, magna sed
+                      pulvinar ultricies, purus lectus malesuada libero, sit
+                      amet commodo magna eros quis urna. Nunc viverra imperdiet
+                      enim. Fusce est. Vivamus a tellus. Pellentesque habitant
+                      morbi tristique senectus et netus et malesuada fames ac
+                      turpis egestas. Proin pharetra nonummy pede. Mauris et
+                      orci. Aenean nec lorem. In porttitor. Donec laoreet
+                      nonummy augue. Suspendisse dui purus, scelerisque at,
+                      vulputate vitae, pretium mattis, nunc. Mauris eget neque
+                      at sem venenatis eleifend. Ut nonummy.
+                    </div>
+
+                    <div class="col-auto"></div>
+                    <div class="col-auto"></div>
+                  </div>
+                  <div class="col-12 col-lg-6"></div>
                 </div>
-                <div id="open-heading" className="1-text heading-text">
-                  At An Affordable Price
-                </div>
-                <div class="flex">
-                  <div class="x1 goldbar home"></div>
-                  <div class="x1"></div>
-                </div>
-                <div className="r-text paragraph">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.
+              </div>
+              <div class="col-sm ">
+                <div class="1-text heading-text d-flex justify-content-end">
+                  <img className="pics" src={Demo1} alt="" />
                 </div>
               </div>
             </div>
