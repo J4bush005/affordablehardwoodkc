70ec76c2667685eff1a9f04c45202a493a3ea311
Johnathon Forbush <jtforbush429@gmail.com>
2023-06-11 16:09:21 -0500

    Making some changes to the animations

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,6 +9,8 @@ import {
   Move,
   FadeIn,
   MoveOut,
+  StickyIn,
+  MoveIn,
   Zoom,
 } from "react-scroll-motion";
 import Logo from "../images/Logo.png";
@@ -20,8 +22,8 @@ import Demo5 from "../images/Demo5.png";
 import Demo6 from "../images/Demo6.png";
 import { Link } from "react-router-dom";
 
-const FadeUp = batch(Fade(), Sticky(38), Move());
-const FadeUp2 = batch(Fade(), Sticky(), Move());
+const FadeUp = batch(FadeIn(0, 1), MoveIn(400, 300), StickyIn(15, 16));
+const MoveUp = batch(FadeIn(0,1), MoveIn(300,200), StickyIn(10, 22));
 
 function Home() {
   return (
@@ -95,52 +97,29 @@ function Home() {
         </button>
       </div>
 
-      <div className="container-fluid">
-        <ScrollPage page={0}>
+      <ScrollPage page={0}>
+        <div className="flex container-fluid">
           <Animator animation={FadeUp}>
             <div class="row">
-              <div class="col-12 col-lg-6">
-                <div class="row">
-                  <div class="col-12">
-                    <div id="open-heading" class="1-text heading-text">
-                      Providing Superior Quality
-                    </div>
-                    <div id="open-heading" class="1-text heading-text">
-                      At An Affordable Price
-                    </div>
-                    <div class="flex">
-                      <div class="x1 goldbar home"></div>
-                      <div class="x1"></div>
-                    </div>
-                    <div class="r-text paragraph">
-                      Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                      Maecenas porttitor congue massa. Fusce posuere, magna sed
-                      pulvinar ultricies, purus lectus malesuada libero, sit
-                      amet commodo magna eros quis urna. Nunc viverra imperdiet
-                      enim. Fusce est. Vivamus a tellus. Pellentesque habitant
-                      morbi tristique senectus et netus et malesuada fames ac
-                      turpis egestas. Proin pharetra nonummy pede. Mauris et
-                      orci. Aenean nec lorem. In porttitor. Donec laoreet
-                      nonummy augue. Suspendisse dui purus, scelerisque at,
-                      vulputate vitae, pretium mattis, nunc. Mauris eget neque
-                      at sem venenatis eleifend. Ut nonummy.
-                    </div>
-
-                    <div class="col-auto"></div>
-                    <div class="col-auto"></div>
-                  </div>
-                  <div class="col-12 col-lg-6"></div>
+              <div class="col">
+                <div id="open-heading" class="1-text heading-text">
+                  Providing Superior Quality
                 </div>
-              </div>
-              <div class="col-sm ">
-                <div class="1-text heading-text d-flex justify-content-end">
-                  <img className="pics" src={Demo1} alt="" />
+                <div id="open-heading" class="1-text heading-text">
+                  At An Affordable Price
                 </div>
               </div>
             </div>
           </Animator>
-        </ScrollPage>
-      </div>
+          <Animator animation={MoveUp}>
+            <div className="flex">
+              <div className="x1 goldbar home">
+                <div className="x1"></div>
+              </div>
+            </div>
+          </Animator>
+        </div>
+      </ScrollPage>
     </ScrollContainer>
   );
 }
