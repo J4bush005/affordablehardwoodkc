60449cb99f39f5df107c91e9725bb200be316852
Johnathon Forbush <jtforbush429@gmail.com>
2023-06-17 22:09:39 -0500

    Making it responsive

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -17,21 +17,28 @@ export const Contact = () => {
 
   return (
     <div className="container-fluid">
-    <form class="row g-3 d-flex justify-content-center" ref={form} onSubmit={sendEmail}>
+    <form  ref={form} onSubmit={sendEmail}>
+    <div class="row g-3 d-flex justify-content-center">
     <div className="col-6">
-      <label class="form-label">Name</label>
+      <label class="form-label">Name:</label>
       <input type="text" class="form-control" placeholder="John Smith"name="user_name" />
       </div>
 
       <div className="row g-3 d-flex justify-content-center">
       <div className="col-6">
-      <label class="form-label">Email</label>
+      <label class="form-label">Email:</label>
       <input type="email" class="form-control" placeholder="JSmith@gmail.com" name="user_email" />
       </div>
       </div>
+      <div className="row g-3 d-flex justify-content-center">
+      <div className="col-6">
+       <label class="form-label" >Message:</label>
+        <textarea className="width d-flex justify-content-center" name="message" />
+       </div>
+       </div>
+     
+      </div>
 
-      <label>Message</label>
-      <textarea name="message" />
       <input type="submit" value="Send" />
     </form>
     </div>
