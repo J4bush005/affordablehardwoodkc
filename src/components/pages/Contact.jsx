998340328c0a25a53f1bdedeedd37533ce9047e6
Johnathon Forbush <jtforbush429@gmail.com>
2023-06-17 21:46:52 -0500

    Added some styling

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -16,15 +16,25 @@ export const Contact = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
+    <div className="container-fluid">
+    <form class="row g-3 d-flex justify-content-center" ref={form} onSubmit={sendEmail}>
+    <div className="col-6">
       <label class="form-label">Name</label>
       <input type="text" class="form-control" placeholder="John Smith"name="user_name" />
+      </div>
+
+      <div className="row g-3 d-flex justify-content-center">
+      <div className="col-6">
       <label class="form-label">Email</label>
       <input type="email" class="form-control" placeholder="JSmith@gmail.com" name="user_email" />
+      </div>
+      </div>
+
       <label>Message</label>
       <textarea name="message" />
       <input type="submit" value="Send" />
     </form>
+    </div>
   );
 };
 
