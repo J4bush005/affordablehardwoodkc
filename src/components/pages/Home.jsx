660ba4dc117480fb8865f1be0675a705ca939784
Johnathon Forbush <jtforbush429@gmail.com>
2023-06-19 20:24:35 -0500

    Working on smaller screens

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -26,185 +26,294 @@ const face = "https://www.facebook.com/profile.php?id=100001292195808";
 const linked = "https://www.linkedin.com/in/johnathon-forbush-a4ba3373/";
 const mine = "https://j4bush005.github.io/";
 
-
 function Home() {
   return (
     <ScrollContainer>
       {/*Image Carousel*/}
       <section className="images">
-      <div
-        id="carouselExampleAutoplaying"
-        class="carousel slide"
-        data-bs-ride="carousel"
-      >
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img
-              src={Demo1}
-              class="d-block img-responsive image-resize"
-              alt=""
-            ></img>
+        <div
+          id="carouselExampleAutoplaying"
+          class="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img
+                src={Demo1}
+                class="d-block img-responsive image-resize"
+                alt=""
+              ></img>
+            </div>
+            <div class="carousel-item">
+              <img
+                src={Demo2}
+                class="d-block img-responsive image-resize"
+                alt=""
+              ></img>
+            </div>
+            <div class="carousel-item">
+              <img
+                src={Demo3}
+                class="d-block img-responsive image-resize"
+                alt=""
+              ></img>
+            </div>
+            <div class="carousel-item">
+              <img
+                src={Demo4}
+                class="d-block img-responsive image-resize"
+                alt=""
+              ></img>
+            </div>
+            <div class="carousel-item">
+              <img
+                src={Demo5}
+                class="d-block img-responsive image-resize"
+                alt=""
+              ></img>
+            </div>
+            <div class="carousel-item">
+              <img
+                src={Demo6}
+                class="d-block img-responsive image-resize"
+                alt=""
+              ></img>
+            </div>
           </div>
-          <div class="carousel-item">
-            <img
-              src={Demo2}
-              class="d-block img-responsive image-resize"
-              alt=""
-            ></img>
-          </div>
-          <div class="carousel-item">
-            <img
-              src={Demo3}
-              class="d-block img-responsive image-resize"
-              alt=""
-            ></img>
-          </div>
-          <div class="carousel-item">
-            <img
-              src={Demo4}
-              class="d-block img-responsive image-resize"
-              alt=""
-            ></img>
-          </div>
-          <div class="carousel-item">
-            <img
-              src={Demo5}
-              class="d-block img-responsive image-resize"
-              alt=""
-            ></img>
-          </div>
-          <div class="carousel-item">
-            <img
-              src={Demo6}
-              class="d-block img-responsive image-resize"
-              alt=""
-            ></img>
-          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleAutoplaying"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleAutoplaying"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
       </section>
 
+      {/*PHONES*/}
+      <Media query="(min-width: 0px) and (max-width: 481px)">
+      <motion.div
+              className="graph-container"
+              initial={{ opacity: 0, x: 9 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1, delay: 0.3 }}
+            >
+        <div className="row sm-screen">
+          <div className="col">
+            <div id="open-heading-sm" className="1-text heading-text">
+              Providing Superior Quality
+            </div>
+            <div className="flex">
+              <div className="x1 goldbar home">
+                <div className="x1"></div>
+              </div>
+            </div>
+            <div className="r-text-sm">
+              {" "}
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas
+              porttitor congue massa. Fusce posuere, magna sed pulvinar
+              ultricies, purus lectus malesuada libero, sit amet commodo magna
+              eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a
+              tellus.
+            </div>
+          </div>
+          <div className="col">
+            <img src={Kids} alt="" className="img-responsive pic x2 flex sm" />
+          </div>
+        </div>
+        </motion.div>
+      </Media>
+      <Media query="(min-width: 0px) and (max-width: 481px)">
+       <motion.div
+              className="graph-container"
+              initial={{ opacity: 0, x: 9 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1, delay: 0.3 }}
+            >
+        <div id="sm-screen" className="row">
+          <div className="col">
+            <div id="open-heading-sm" className="1-text heading-text">
+              At An Affordable Price
+            </div>
+            <div className="flex small">
+              <div className="x1 goldbar home">
+                <div className="x1"></div>
+              </div>
+            </div>
+            <div className="r-text">
+              {" "}
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas
+              porttitor congue massa. Fusce posuere, magna sed pulvinar
+              ultricies, purus lectus malesuada libero, sit amet commodo magna
+              eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a
+              tellus.
+            </div>
+          </div>
+          <div className="col">
+            <img
+              src={Buffer}
+              alt=""
+              className="img-responsive pic x2 flex sms"
+            />
+          </div>
+        </div>
+        </motion.div>
+      </Media>
+      <Media query="(min-width: 0px) and (max-width: 481px)">
+      <motion.div
+              className="graph-container"
+              initial={{ opacity: 0, x: -9 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1, delay: 0.3 }}
+            >
+        <div className="row screen">
+          <div className="col">
+            <div id="third-open-heading-sm" className="1-text-heading-text">
+              Meet The HardwoodBoss
+            </div>
+            <div className="flex">
+              <div className="x1 goldbar home">
+                <div className="x1"></div>
+              </div>
+            </div>
+            <div className="r-text-sm">
+              {" "}
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas
+              porttitor congue massa. Fusce posuere, magna sed pulvinar
+              ultricies, purus lectus malesuada libero, sit amet commodo magna
+              eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a
+              tellus.
+            </div>
+          </div>
+          <div className="col">
+            <img
+              src={Rappers}
+              alt=""
+              id="third-image-small"
+              className="img-responsive pic x2 flex small"
+            />
+          </div>
+        </div>
+        </motion.div>
+      </Media>
+
+      <Media query="(min-width: 1900px)">
       <div className="flex container-fluid">
         <div className="row">
           <section className="paragraph1">
-          <motion.div
-          className="graph-container"
-          initial={{opacity: 0, x: -900}}
-          whileInView={{opacity: 1, x: 0}}
-          transition={{duration: 1}}
-      
-          >
-            <div className="row justify-centent-evenly open">
-              <div className="col-6">
-                <div id="open-heading" className="1-text heading-text">
-                  Providing Superior Quality
-                </div>
-                <div className="flex">
-                  <div className="x1 goldbar home">
-                    <div className="x1"></div>
+            <motion.div
+              className="graph-container"
+              initial={{ opacity: 0, x: -900 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1, delay: 0.3 }}
+            >
+              <div className="row justify-centent-evenly open">
+                <div className="col-6">
+                  <div id="open-heading" className="1-text heading-text">
+                    Providing Superior Quality
+                  </div>
+                  <div className="flex">
+                    <div className="x1 goldbar home">
+                      <div className="x1"></div>
+                    </div>
+                  </div>
+                  <div className="r-text">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Maecenas porttitor congue massa. Fusce posuere, magna sed
+                    pulvinar ultricies, purus lectus malesuada libero, sit amet
+                    commodo magna eros quis urna. Nunc viverra imperdiet enim.
+                    Fusce est. Vivamus a tellus.
                   </div>
                 </div>
-                <div className="r-text">
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                  Maecenas porttitor congue massa. Fusce posuere, magna sed
-                  pulvinar ultricies, purus lectus malesuada libero, sit amet
-                  commodo magna eros quis urna. Nunc viverra imperdiet enim.
-                  Fusce est. Vivamus a tellus.
+                <div className="col-6">
+                  <img src={Kids} alt="" className="img-fluid pic x2 flex" />
                 </div>
               </div>
-              <div className="col-6">
-                <img src={Kids} alt="" className="img-fluid pic x2 flex" />
-              </div>
-            </div>
             </motion.div>
           </section>
 
           <section className="paragraph2">
-          <motion.div
-          className="graph-container"
-          initial={{opacity: 0, x: 900}}
-          whileInView={{opacity: 1, x: 0}}
-          transition={{duration: 1}}
-      
-          >
-            <div className="row justify-content-evenly lasts">
-              <div className="col-6">
-                <img src={Buffer} alt="" className="img-fluid x2 flex second" />
-              </div>
-              <div id="test" className="col-6">
-                <div id="open-headings" className="1-text heading-text">
-                  At An Affordable Price
+            <motion.div
+              className="graph-container"
+              initial={{ opacity: 0, x: 900 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1, delay: 0.5 }}
+            >
+              <div className="row justify-content-evenly lasts">
+                <div className="col-6">
+                  <img
+                    src={Buffer}
+                    alt=""
+                    className="img-fluid x2 flex second"
+                  />
                 </div>
-                <div className="flex">
-                  <div className="x1 goldbar home">
-                    <div className="x1"></div>
+                <div id="test" className="col-6">
+                  <div id="open-headings" className="1-text heading-text">
+                    At An Affordable Price
+                  </div>
+                  <div className="flex">
+                    <div className="x1 goldbar home">
+                      <div className="x1"></div>
+                    </div>
+                  </div>
+                  <div id="second-paragraph" className="r-text">
+                    {" "}
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Maecenas porttitor congue massa. Fusce posuere, magna sed
+                    pulvinar ultricies, purus lectus malesuada libero, sit amet
+                    commodo magna eros quis urna. Nunc viverra imperdiet enim.
+                    Fusce est. Vivamus a tellus.{" "}
                   </div>
                 </div>
-                <div id="second-paragraph" className="r-text">
-                  {" "}
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                  Maecenas porttitor congue massa. Fusce posuere, magna sed
-                  pulvinar ultricies, purus lectus malesuada libero, sit amet
-                  commodo magna eros quis urna. Nunc viverra imperdiet enim.
-                  Fusce est. Vivamus a tellus.{" "}
-                </div>
               </div>
-            </div>
             </motion.div>
           </section>
 
           <section className="paragraph3">
-           <motion.div
-          className="graph-container"
-          initial={{opacity: 0, x: -900}}
-          whileInView={{opacity: 1, x: 0}}
-          transition={{duration: 1}}
-      
-          >
-            <div className="row justify-centent-evenly open">
-              <div className="col-6">
-                <div id="open-heading" className="1-text heading-text">
-                  Meet The Hardwood Boss
-                </div>
-                <div className="flex">
-                  <div className="x1 goldbar home">
-                    <div className="x1"></div>
+            <motion.div
+              className="graph-container"
+              initial={{ opacity: 0, x: -900 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1, delay: 0.3 }}
+            >
+              <div className="row justify-centent-evenly open">
+                <div className="col-6">
+                  <div id="open-heading" className="1-text heading-text">
+                    Meet The Hardwood Boss
+                  </div>
+                  <div className="flex">
+                    <div className="x1 goldbar home">
+                      <div className="x1"></div>
+                    </div>
+                  </div>
+                  <div className="r-text">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Maecenas porttitor congue massa. Fusce posuere, magna sed
+                    pulvinar ultricies, purus lectus malesuada libero, sit amet
+                    commodo magna eros quis urna. Nunc viverra imperdiet enim.
+                    Fusce est. Vivamus a tellus.
                   </div>
                 </div>
-                <div className="r-text">
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                  Maecenas porttitor congue massa. Fusce posuere, magna sed
-                  pulvinar ultricies, purus lectus malesuada libero, sit amet
-                  commodo magna eros quis urna. Nunc viverra imperdiet enim.
-                  Fusce est. Vivamus a tellus.
+                <div className="col-6">
+                  <img src={Kids} alt="" className="img-fluid pic x2 flex" />
                 </div>
               </div>
-              <div className="col-6">
-                <img src={Kids} alt="" className="img-fluid pic x2 flex" />
-              </div>
-            </div>
             </motion.div>
           </section>
         </div>
       </div>
+      </Media>
 
       {/* Footer */}
       <div class="container my-5 foot">
