8dda095bfb48677959822bcd77ebc613e284148d
Johnathon Forbush <jtforbush429@gmail.com>
2023-06-20 22:16:57 -0500

    Finished Thank you Page for now

--- src/components/pages/ThankYou.jsx
+++ src/components/pages/ThankYou.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
 function ThankYou() {
-    return(
-        <div className="container">
-            <div className="card mt-4">
-                <div className="card-body">
-                    <h2>Thank You Page</h2>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="row confirm">
+        <div className="col">Message Successfully Sent!</div>
+      </div>
+      <div className="row thank-you">
+        <div className="col-9">
+          Thank you so much for contacting Affordable Hardwood Kansas City! Any
+          question, comment, or concern you might have is currently our number
+          one priority, and we will get back with you as quickly as we can. We
+          appericiate your support and have a blessed day.
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default ThankYou;
