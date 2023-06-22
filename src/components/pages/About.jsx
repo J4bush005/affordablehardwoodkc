7b9aebf972d04053e9a78b5f56027b05ea92590c
Johnathon Forbush <jtforbush429@gmail.com>
2023-06-22 17:49:00 -0500

    Working on about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -15,7 +15,17 @@ function About() {
       </section>
       <section className="page-section call-to-action d-flex">
         <div className="container-fluid">
-            
+          <div className="row">
+            <div className="col">
+              <p className="text-center rev mt-3">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Maecenas porttitor congue massa. Fusce posuere, magna sed
+                pulvinar ultricies, purus lectus malesuada libero, sit amet
+                commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce
+                est. Vivamus a tellus.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </>
