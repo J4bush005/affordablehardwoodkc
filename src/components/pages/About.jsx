37da1ac5c647f78529eb194563eb2534e010b14f
Johnathon Forbush <jtforbush429@gmail.com>
2023-06-25 21:19:32 -0500

    Working on career page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1029,6 +1029,24 @@ function About() {
                         >
                           Sander
                         </td>
+                        <section
+                          id="Sander-desc"
+                          className="collapse multi-collapse"
+                        >
+                          <div className="row">
+                            <div className="col">
+                              <div className="second-word">
+                                {" "}
+                                Lorem ipsum dolor sit amet, consectetuer
+                                adipiscing elit. Maecenas porttitor congue
+                                massa. Fusce posuere, magna sed pulvinar
+                                ultricies, purus lectus malesuada libero, sit
+                                amet commodo magna eros quis urna. Nunc viverra
+                                imperdiet enim. Fusce est. Vivamus a tellus.
+                              </div>
+                            </div>
+                          </div>
+                        </section>
                       </tr>
                     </tbody>
                   </table>
