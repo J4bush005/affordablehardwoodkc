bdf3a65f8f3339b97d3cec2582e28b9ad8e2b367
Johnathon Forbush <jtforbush429@gmail.com>
2023-07-02 17:32:13 -0500

    Adding heading video

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import ScrollToTop from "../ScrollToTop";
+import Install from "../videos/Install 2.mp4";
 
 function Services() {
-    return(
-        <div className="container">
-            <div className="card mt-4">
-                <div className="card-body">
-                    <h2>Services Page</h2>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div >
+      <ScrollToTop />
+         <section
+         id="main"
+              className="page-section call-to-action d-flex"
+            >
+             <video id="install-video" src={Install} autoPlay loop muted></video>
+            </section>
+
+    </div>
+  );
 }
 
 export default Services;
