a9f33292aeb4e820737e9e150c990578c6997612
Johnathon Forbush <jtforbush429@gmail.com>
2023-07-02 21:45:55 -0500

    Added footer

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ScrollToTop from "../ScrollToTop";
+import { Link } from "react-router-dom";
 import Install from "../videos/Install 2.mp4";
 import Stain from "../videos/Stain Video.mp4";
+import Insta from "../images/ig logo.png";
+import Face from "../images/face.png";
+import Linked from "../images/Linked.png";
 import Media from "react-media";
 import Misc from "../videos/Misc.mp4";
 import { motion } from "framer-motion";
+
+
+const insta = "https://www.instagram.com/hardwoodboss_/";
+const face = "https://www.facebook.com/profile.php?id=100001292195808";
+const linked = "https://www.linkedin.com/in/johnathon-forbush-a4ba3373/";
+const mine = "https://j4bush005.github.io/";
 
 function Services() {
   return (
@@ -415,9 +425,117 @@ function Services() {
             </section>
              </motion.div>
           </section>
+
+          
          
         </div>
       </Media>
+       <div class="container my-5 foot">
+        <footer class="text-center text-white">
+          <div class="container">
+            <section class="mt-5">
+              <div class="row text-center d-flex justify-content-center">
+                <div class="col-md-2">
+                  <h6 class="text-uppercase font-weight-bold">
+                    <Link to="/About" class="foot-hover">
+                      About us
+                    </Link>
+                  </h6>
+                </div>
+
+                <div class="col-md-2">
+                  <h6 class="text-uppercase font-weight-bold">
+                    <Link to="/Services" class="foot-hover">
+                      Services
+                    </Link>
+                  </h6>
+                </div>
+                <div class="col-md-2">
+                  <h6 class="text-uppercase font-weight-bold">
+                    <Link to="/About" class="foot-hover">
+                      Reviews
+                    </Link>
+                  </h6>
+                </div>
+
+                <div class="col-md-2">
+                  <h6 class="text-uppercase font-weight-bold">
+                    <Link to="/About" class="foot-hover">
+                      Find a Career
+                    </Link>
+                  </h6>
+                </div>
+                <div class="col-md-2">
+                  <h6 class="text-uppercase font-weight-bold">
+                    <Link to="/Contact" class="foot-hover">
+                      Contact
+                    </Link>
+                  </h6>
+                </div>
+              </div>
+            </section>
+
+            <hr class="my-5" />
+            <section class="mb-4 move">
+              <Link
+                to="/"
+                class="btn btn-floating m1"
+                role="button"
+                aria-expanded="false"
+              >
+                <i class="fab fa-instagram">
+                  <img
+                    src={Insta}
+                    class="img-fluid hover"
+                    onClick={() => window.open(insta, "_blank")}
+                    alt=""
+                  />
+                </i>
+              </Link>
+              <Link
+                to="/"
+                class="btn btn-floating m1"
+                role="button"
+                aria-expanded="false"
+              >
+                <i class="fab fa-facebook-f">
+                  <img
+                    src={Face}
+                    class="img-fluid hover"
+                    onClick={() => window.open(face, "_blank")}
+                    alt=""
+                  />
+                </i>
+              </Link>
+              <Link
+                to="/"
+                class="btn btn-floating m1"
+                role="button"
+                aria-expanded="false"
+              >
+                <i class="fab fa-linkedin">
+                  <img
+                    src={Linked}
+                    class="img-fluid hover"
+                    onClick={() => window.open(linked, "_blank")}
+                    alt=""
+                  />
+                </i>
+              </Link>
+            </section>
+          </div>
+          <div class="text-center p-3">
+            Designed by:
+            <Link
+              to="/"
+              class="foot-hover space"
+              onClick={() => window.open(mine, "_blank")}
+            >
+              Forbush Technologies LLC{" "}
+            </Link>
+          </div>
+        </footer>
+      </div>
     </>
   );
 }
