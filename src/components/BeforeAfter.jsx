cb1c059ea85872edc345d8ff9fb03ec98195e3c3
Johnathon Forbush <jtforbush429@gmail.com>
2023-07-12 00:55:33 -0500

    Working on gallery page

---
+++ src/components/BeforeAfter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScrollToTop from "../ScrollToTop";
+
+const BeforeAfter = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default BeforeAfter;
