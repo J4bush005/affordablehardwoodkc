31d52ff2ec6a9686f3b30ecc177e4d808ec75189
j4bush005 <jtforbush429@gmail.com>
2023-09-24 01:10:03 -0500

    Redoing the gallery page

--- src/components/pages/Custom.jsx
+++ src/components/pages/Custom.jsx
@@ -9,111 +9,165 @@ import Custom7 from "../images/Custom Floor 6.webp";
 import Custom8 from "../BeforeAfterPics/DB26.webp";
 import Custom9 from "../BeforeAfterPics/DB27.webp";
 import Custom11 from "../BeforeAfterPics/DB33.webp";
+import { Link } from 'react-router-dom';
+import Footer from '../Footer';
 
 function Custom() {
   return (
     <section id="custom-pictures">
-    <section
-      id="custom"
-      className="page-section call-to-action d-flex mt-3"
-    >
-      <div className="py-lg-5">
-        <div className="col-lg-12 col-md-8 mx-auto">
-          <h1 className="mb-5">Custom Designs</h1>
+        <section id="join-fam">
+        <div className="">
+          <div className="col">
+            <div id="care-headings" className="1-text heading-text">
+              Custom Floors
+            </div>
+            <div className="flex">
+              <div className="x1 goldbar home news">
+                <div className="x2"></div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+        <div className="">
+          <div className="col">
+            <div id="" className="second-word">
+              <ul className="options">
+                <li className="listed-one">
+                  <Link to={"/Classic"} className="sections">Classic</Link>
+                </li>
+                <li className="listed-two">|</li>
+                <li className="listed-three">
+                  <Link to={"/LVT"} className="sections">LVT</Link>
+                </li>
+                <li className="listed-four">|</li>
+                <li className="listed-five">
+                  <Link to={"/Commercial"} className="sections">Commercial</Link>
+                </li>
+                <li className="listed-six">|</li>
+                <li className="listed-seven">
+                  <Link to={"/Custom"} className="sections">Custom</Link>
+                </li>
+                <li className="listed-eight">|</li>
+                <li className="listed-nine">
+                  <Link to={"/Stairs"} className="sections">Stairs</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
 
     <section id="row-clasic">
       <div className="d-flex">
-        <div className="col-4">
+        <div className="col">
+        <div id="picture-widths">
           <img
-            id="classic-img"
+            id=""
             src={Customs}
             alt=""
             className="img-fluid img-thumbnail"
           />{" "}
+          </div>
         </div>
-        <div className="col-4">
+        <div className="col">
+        <div id="picture-widths">
           <img
-            id="classic-img-cus"
-            src={Custom2}
+            id=""
+            src={Custom7}
             alt=""
             className="img-fluid img-thumbnail"
           />
+          </div>
         </div>
-        <div className="col-4">
+        <div className="col">
+        <div id="picture-widths">
           <img
-            id="classic-img"
+            id=""
             src={Custom3}
             alt=""
             className="img-fluid img-thumbnail"
           />
+          </div>
         </div>
       </div>
-      <div className="d-flex">
-        <div className="col-4">
+      <div className="d-flex mt-3">
+        <div className="col">
+        <div id="picture-widths">
           <img
-            id="classic-img"
+            id=""
             src={Custom4}
             alt=""
             className="img-fluid img-thumbnail"
           />{" "}
+          </div>
         </div>
-        <div className="col-4">
+        <div className="col">
+        <div id="picture-widths">
           <img
-            id="classic-img"
+            id=""
             src={Custom5}
             alt=""
             className="img-fluid img-thumbnail"
           />
+          </div>
         </div>
-        <div className="col-4">
+        <div className="col">
+        <div id="picture-widths">
           <img
-            id="classic-img"
+            id=""
             src={Custom6}
             alt=""
             className="img-fluid img-thumbnail"
           />
+          </div>
         </div>
         </div>
-        <div className="d-flex">
-        <div className="col-4">
+        <div className="d-flex mt-3">
+        <div className="col">
+        <div id="picture-widths">
           <img
-            id="classic-img"
-            src={Custom7}
+            id=""
+            src={Custom2}
             alt=""
             className="img-fluid img-thumbnail"
           />{" "}
+          </div>
         </div>
-        <div className="col-4">
+        <div className="col">
+        <div id="picture-widths">
           <img
-            id="classic-img-cus"
+            id=""
             src={Custom8}
             alt=""
             className="img-fluid img-thumbnail"
           />
+          </div>
         </div>
-        <div className="col-4">
+        <div className="col">
+        <div id="picture-widths">
           <img
-            id="classic-img"
+            id=""
             src={Custom9}
             alt=""
             className="img-fluid img-thumbnail"
           />
+          </div>
         </div>
       </div>
-       <div className="d-flex">
-        <div className="col-4">
+       <div className="d-flex mt-3">
+        <div className="col">
+        <div id="picture-widths">
           <img
-            id="classic-img-cust"
+            id=""
             src={Custom11}
             alt=""
             className="img-fluid img-thumbnail"
           />
+          </div>
         </div>
       </div>
     </section>
+    <Footer/>
   </section>
   )
 }
--- src/components/pages/Stairs.jsx
+++ src/components/pages/Stairs.jsx
@@ -4,63 +4,106 @@ import Floor21 from "../BeforeAfterPics/DB4.webp";
 import Stair2 from "../images/Stairs2.webp";
 import Stair3 from "../images/Stairs.webp";
 import Stair from "../images/IMG_0286.webp";
+import { Link } from 'react-router-dom';
 
 function Stairs() {
   return (
     <section id="stair-pictures">
-    <section
-      id="stair"
-      className="page-section call-to-action d-flex mt-3"
-    >
-      <div className="py-lg-5">
-        <div className="col-lg-12 col-md-8 mx-auto">
-          <h1 className="mb-5">Stairs</h1>
+       <section id="join-fam">
+        <div className="">
+          <div className="col">
+            <div id="care-headings" className="1-text heading-text">
+              Classic Floors
+            </div>
+            <div className="flex">
+              <div className="x1 goldbar home news">
+                <div className="x2"></div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+        <div className="">
+          <div className="col">
+            <div id="" className="second-word">
+              <ul className="options">
+                <li className="listed-one">
+                  <Link to={"/Classic"} className="sections">Classic</Link>
+                </li>
+                <li className="listed-two">|</li>
+                <li className="listed-three">
+                  <Link to={"/LVT"} className="sections">LVT</Link>
+                </li>
+                <li className="listed-four">|</li>
+                <li className="listed-five">
+                  <Link to={"/Commercial"} className="sections">Commercial</Link>
+                </li>
+                <li className="listed-six">|</li>
+                <li className="listed-seven">
+                  <Link to={"/Custom"} className="sections">Custom</Link>
+                </li>
+                <li className="listed-eight">|</li>
+                <li className="listed-nine">
+                  <Link to={"/Stairs"} className="sections">Stairs</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+
     <section id="row-clasic">
       <div className="d-flex">
-        <div className="col-4">
+        <div className="col">
+        <div id="picture-widths">
           <img
-            id="classic-img"
+            id=""
             src={Stair}
             alt=""
             className="img-fluid img-thumbnail"
           />{" "}
+          </div>
         </div>
-        <div className="col-4">
+        <div className="col">
+        <div id="picture-widths">
           <img
-            id="classic-img-cus"
-            src={Stair2}
-            alt=""
-            className="img-fluid img-thumbnail"
-          />
-        </div>
-        <div className="col-4">
-          <img
-            id="classic-img"
+            id=""
             src={Stair3}
             alt=""
             className="img-fluid img-thumbnail"
           />
+          </div>
+        </div>
+        <div className="col">
+        <div id="picture-widths">
+          <img
+            id=""
+            src={Stair2}
+            alt=""
+            className="img-fluid img-thumbnail"
+          />
+          </div>
         </div>
       </div>
-       <div className="d-flex">
+       <div className="d-flex mt-3">
         <div className="col-4">
+        <div id="picture-widths">
           <img
-            id="classic-img-cust"
+            id=""
             src={Floor21}
             alt=""
             className="img-fluid img-thumbnail"
           />
+          </div>
         </div>
-        <div className="col-4">
+        <div className="col">
+        <div id="picture-widths">
           <img
-            id="classic-img-cust"
+            id=""
             src={Floor20}
             alt=""
             className="img-fluid img-thumbnail"
           />
+          </div>
         </div>
         </div>
      
