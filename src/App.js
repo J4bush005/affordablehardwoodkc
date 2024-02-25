3b23ae573a7fb1f383df2f30234cf6fd09183f8f
j4bush005 <jtforbush429@gmail.com>
2024-02-25 17:11:57 -0600

    Completely redoing the site

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React from 'react';
-import Header from './Header'
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Contact from './components/pages/Contact';
-import Gallery from './components/pages/Gallery';
-import Services from './components/pages/Services';
-import ThankYou from './components/pages/ThankYou';
-import Application from './components/pages/Application';
-import { Route, Routes } from 'react-router-dom';
-import { Estimate } from './components/pages/Estimate';
-import AutoScroll from './components/AutoScroll';
-import Classic from './components/pages/Classic';
-import LVT from './components/pages/LVT';
-import Commercial from './components/pages/Commercial';
-import Custom from './components/pages/Custom';
-import Stairs from './components/pages/Stairs';
+import { Route, Routes } from 'react-router';
+import Header from './components/Header';
+
 
 function App() {
   return (
    <div>
-   <AutoScroll />
-   <Header />
+   <Header/>
    <Routes>
-   <Route index element={<Home />} />
-   <Route path="/About" element={<About />} />
-   <Route path="/Contact" element={<Contact />} />
-   <Route path="/Gallery" element={<Gallery />} />
-   <Route path="/Services" element={<Services />} />
-   <Route path="/ThankYou" element={<ThankYou />} />
-   <Route path="/Application" element={<Application />} />
-   <Route path="/Estimate" element={<Estimate />} />
-   <Route path='/Classic' element={<Classic/>}/>
-   <Route path='/LVT' element={<LVT/>}/>
-   <Route path='/Commercial' element={<Commercial/>}/>
-   <Route path='/Custom' element={<Custom/>}/>
-   <Route path='/Stairs' element={<Stairs/>}/>
    </Routes>
    </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,82 @@
+import "../index.css";
+import React from "react";
+import Logo from "../Photos/Logo.webp"
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <Link to="/" id="logo-img" className="navbar-brand">
+          <img id="logo" className="img-fluid" src={Logo} alt="" />
+        </Link>
+        <Link to="/" id="words" className="navbar-brand">
+          <h1 className="1-text heading-text fs-2 bigger up">
+            Affordable Hardwood
+          </h1>
+          <h1 className="1-text heading-text fs-2 bigger">Kansas City</h1>
+          <div className="flex">
+            <div className="x1 goldbar"></div>
+            <div className="x1"></div>
+          </div>
+          <h2 className="1-text heading-text">
+            Kansas City's Hardwood Specialist
+          </h2>
+        </Link>
+
+        <button
+          className="navbar-toggler custom-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target=".head.collapse"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="head collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav ms-auto d-flex">
+            <li className="nav-item dropdown mx-3">
+              <Link
+                to="/Gallery"
+                id="sub"
+                className="nav-link fs-5"
+                role="button"
+              >
+                Gallery
+              </Link>
+            </li>
+            <li className="nav-item dropdown mx-3 fs-5">
+              <Link to="/About-Us" id="sub" className="nav-link" role="button">
+                About Us
+              </Link>
+            </li>
+            <li className="nav-item dropdown mx-3 fs-5">
+              <Link
+                to="/Request-An-Estimate"
+                id="sub"
+                className="nav-link"
+                role="button"
+              >
+                Request an Estimate
+              </Link>
+            </li>
+            <li className="nav-item dropdown mx-3 fs-5">
+              <Link
+                to="/Contact-Us"
+                id="sub"
+                className="nav-link"
+                role="button"
+              >
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
