d6030631dac63d739851f9650d64621a8eff52d8
j4bush005 <jtforbush429@gmail.com>
2024-02-25 18:39:27 -0600

    Completely redoing the site

--- src/components/Pages/Home/Story.jsx
+++ src/components/Pages/Home/Story.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import Kids from "../../../Photos/kids.webp";
 function Story() {
   return (
-    <div>Story</div>
-  )
+    <div>
+      <div className="container mt-5">
+        <div className="header display-5">Our Story</div>
+        <div className="row d-flex">
+          <div className="col-lg-6 col-md-12 col-sm-12 fs-4" id="story">
+            Johnathon Forbush founded Affrodable Hardwood Kansas City with a commitment to
+            providing top-notch hardwood services. For more than 25 years
+            Affordable Hardwood has ensured precision and expertise in every
+            project. We pride ourselves on exceptional service and results. We
+            exceed expectations with unparalleled craftsmanship and attention to
+            detail with customer satisfaction being our highest priority.
+          </div>
+          <div id="kid-picture" className="col-lg-6 col-md-12 col-sm-12">
+            <img src={Kids} alt="Boss" className="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Story
+export default Story;
